2bd0dd62f5e0199621e837f0d14da02b8befaf05
Jim Nicholson <nicholsonsoftware@hotmail.com>
2018-10-16 07:16:23 -0400

    Added CREATE table sql script

---
+++ PatientManagement/App_Data/Create dbo.Patient.sql
@@ -0,0 +1,24 @@
+﻿USE [PatientManagement]
+GO
+
+/****** Object: Table [dbo].[Patient] Script Date: 10/16/2018 7:15:50 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Patient] (
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]       NVARCHAR (MAX) NOT NULL,
+    [LastName]        NVARCHAR (MAX) NOT NULL,
+    [Phone]           NVARCHAR (MAX) NOT NULL,
+    [Email]           NVARCHAR (MAX) NOT NULL,
+    [Gender]          NVARCHAR (MAX) NOT NULL,
+    [Notes]           TEXT           NULL,
+    [CreatedDate]     DATETIME2 (7)  NOT NULL,
+    [LastUpdatedDate] DATETIME2 (7)  NOT NULL,
+    [IsDeleted]       BIT            NOT NULL
+);
+
+
